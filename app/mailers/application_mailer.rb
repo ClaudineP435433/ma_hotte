257d5428ca3c9b8afe66d140d1981e4bfae51a19
Claudine Pradal <claudinecartailler@gmail.com>
2018-06-27 10:25:32 +0200

    change mail from sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'mahotte.fr@gmail.com'
+  #default from: 'mahotte.fr@gmail.com'
+  default from: 'contact@mahotte.fr'
   layout 'mailer'
 end
